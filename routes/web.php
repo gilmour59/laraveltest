4e0e895610a57a597c20469ebde3055e0c2d8d70
gilmour59 <gilmouralmalbisdev@gmail.com>
2018-08-23 16:49:24 +0800

    home changes

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ Route::get('/about', 'PagesController@about');
 Route::get('/services', 'PagesController@services');
 
 Route::resource('post', 'PostsController');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
